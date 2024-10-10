2180a548994f597891b70bc9279d20bf4a43c795
Jenittah03 <iamjenittahs@gmail.com>
2024-10-10 11:11:40 +0530

    2nd ques commit

---
+++ FitnessProgramManagementSystem/FitnessProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class FitnessProgram
+    {
+        public int FitnessProgramId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
