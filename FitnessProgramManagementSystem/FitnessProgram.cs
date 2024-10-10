c3a3a84d5e06b6524a7cc5f3641f235df648314e
Jenittah03 <iamjenittahs@gmail.com>
2024-10-10 11:17:59 +0530

    3rd ques commit

--- FitnessProgramManagementSystem/FitnessProgram.cs
+++ FitnessProgramManagementSystem/FitnessProgram.cs
@@ -12,5 +12,20 @@ namespace FitnessProgramManagementSystem
         public string Title { get; set; }
         public string Duration { get; set; }
         public decimal Price { get; set; }
+
+        public FitnessProgram(int fitnessProgramId, string title, string duration, decimal price)
+        {
+            FitnessProgramId = fitnessProgramId;
+            Title = title;
+            Duration = duration;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {FitnessProgramId}, Title: {Title}, Duration: {Duration}, Price: {Price}";
+        }
+
+
     }
 }
